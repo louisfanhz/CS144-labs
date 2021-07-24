d94cbd39c6f28f867831515eb713ffa9b8350f66
Haozhi Fan <louisfanhz@gmail.com>
2021-07-24 09:14:43 -0700

    lab1 done

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -2,13 +2,6 @@
 
 #include <string>
 
-// Dummy implementation of a flow-controlled in-memory byte stream.
-
-// For Lab 0, please replace with a real implementation that passes the
-// automated checks run by `make check_lab0`.
-
-// You will need to add private members to the class declaration in `byte_stream.hh`
-
 template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
 
